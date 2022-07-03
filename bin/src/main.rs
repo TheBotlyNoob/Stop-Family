30c4a27b21658dabeb611ae7e7e87defa878a915
Jay Jackson <jj@jj.is-a.dev>
2022-07-03 15:40:59 -0400

    update

--- bin/src/main.rs
+++ bin/src/main.rs
@@ -1,6 +1,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use std::ptr;
+use walkdir::WalkDir;
 use windows::Win32::{
     Foundation::{HANDLE, LUID},
     Security::{
@@ -12,8 +13,6 @@ use windows::Win32::{
 };
 
 mod kill;
-
-const WPCMON: &str = "WPCMon.exe";
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
@@ -65,24 +64,33 @@ fn main() -> Result<()> {
             }
         }
 
-        println!("[+] Killing WPCMon...");
-        kill::by_name(WPCMON)?;
+        for wpc in WalkDir::new(r"C:\Windows\System32")
+            .into_iter()
+            .filter_map(|r| r.ok())
+            .filter(|r| {
+                r.file_name().to_str().map(|s| {
+                    let s = s.to_lowercase();
+                    s.starts_with("wpc") && !s.ends_with("bak")
+                }) == Some(true)
+            })
+        {
+            kill::by_name(wpc.file_name().to_str().unwrap())?;
 
-        let mut exit_code = 0;
-
-        let wpcmon_path = format!(r"C:\Windows\System32\{}", WPCMON);
-        if let Err(e) = std::fs::rename(&wpcmon_path, format!("{}.bak", wpcmon_path)) {
-            if e.kind() != std::io::ErrorKind::NotFound {
-                println!("[!] Failed to delete {wpcmon_path}: {e:#?}.");
-                exit_code = e.raw_os_error().unwrap_or(1);
+            if let Err(e) = std::fs::rename(&wpc.path(), wpc.path().with_extension("bak")) {
+                if e.kind() != std::io::ErrorKind::NotFound {
+                    println!(
+                        "[!] Failed to rename {}: {e}.",
+                        wpc.file_name().to_string_lossy()
+                    );
+                }
+            } else {
+                println!("[+] Renamed {}.", wpc.file_name().to_string_lossy());
             }
-        } else {
-            println!("[+] Deleted {wpcmon_path}.");
         }
 
         println!("[+] Finished. Closing in 5 seconds.");
         std::thread::sleep(std::time::Duration::from_secs(5));
 
-        std::process::exit(exit_code);
+        Ok(())
     }
 }
